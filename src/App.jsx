3b0479d703c8f2e38632731294864bbb791a56dc
Ashan <ashanvimodh241@gmail.com>
2025-10-16 22:04:50 +0530

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 
 function App() {
 
   return (
     <>
+      <HeaderComponent/>
       <ListEmployeeComponent/>
     </>
   )
