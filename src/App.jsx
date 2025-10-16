6be8140b93e15ae50418bb5abd81d83eb847cf81
Ashan <ashanvimodh241@gmail.com>
2025-10-17 05:02:52 +0530

    configure initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <HeaderComponent/>
-      <ListEmployeeComponent/>
-      <FooterComponent/>
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+
+          {/* http://localhost:3000 */}
+          <Route path='/' element={<ListEmployeeComponent />}></Route>
+
+          {/* http://localhost:3000/employes */}
+          <Route path='/employes' element={<ListEmployeeComponent />}></Route>
+
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
